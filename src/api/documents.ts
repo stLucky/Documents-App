a5dac5862cc0a673cfc1e1dc5fd247bb6922210d
stLucky <asys4759@gmail.com>
2022-11-17 14:34:57 +0300

    adds env variables

--- src/api/documents.ts
+++ src/api/documents.ts
@@ -3,7 +3,7 @@ import type { Documents } from "./types/documents";
 export const getDocuments = async (
   search?: string
 ): Promise<Documents.Response> => {
-  const url = new URL("https://agile-sands-40710.herokuapp.com/user/docs");
+  const url = new URL(`${import.meta.env.VITE_APP_API_URL}/user/docs`);
   url.searchParams.set("search", `${search}`);
   const response = await fetch(url);
 
